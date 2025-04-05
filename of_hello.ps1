646a1a5d42047ce41c7842f9d2f9b18f86390a8e
Deep-Chandra-Mathpal <59234542+Deep-Chandra-Mathpal@users.noreply.github.com>
2025-04-05 16:23:04 +0530

    Update of_hello.ps1

--- of_hello.ps1
+++ of_hello.ps1
@@ -1,3 +1,1 @@
-& ([string]::Join('', 'W','r','i','t','e','-','O','u','t','p','u','t')) (
-    ([char]72)+([char]101)+([char]108)+([char]108)+([char]111)+' fr'+'om a downlo'+'aded scrip'+'t!'
-)
+& ([string]::Join('', 'W','r','i','t','e','-','O','u','t','p','u','t')) (([char]72)+([char]101)+([char]108)+([char]108)+([char]111)+' fr'+'om a downlo'+'aded scrip'+'t!')
